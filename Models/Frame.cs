6cffa178e99f6e09a6616590c436fe8e188036eb
GurjeetSingh007 <W0792908@myscc.ca>
2022-10-15 14:50:27 -0700

    Created Frame class model

---
+++ Models/Frame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorldwideFrames.Models
+{
+    public class Frame
+    {
+        public int Id { get; set; }
+        public string FrameType { get; set; }
+        public string FrameStyle { get; set; }
+        public string Color { get; set; }
+
+
+        public decimal FrameHeight { get; set; }
+        public decimal FrameWidth { get; set; }
+        public decimal FramePrice { get; set; }
+    }
+}
