67b7f232713a9914698470e0a8df73a4a226bd31
GurjeetSingh007 <W0792908@myscc.ca>
2022-10-15 15:19:32 -0700

    Created Scaffolded Item according frame model

---
+++ Migrations/20221015220854_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WorldwideFrames.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Frame",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FrameType = table.Column<string>(nullable: true),
+                    FrameStyle = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    FrameHeight = table.Column<decimal>(nullable: false),
+                    FrameWidth = table.Column<decimal>(nullable: false),
+                    FramePrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Frame", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Frame");
+        }
+    }
+}
