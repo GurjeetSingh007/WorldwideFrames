2dd94fee88f08689de5a7e553ad2280eba7541a7
GurjeetSingh007 <W0792908@myscc.ca>
2022-10-15 14:56:47 -0700

    Added database context for WorldwideFrames

---
+++ Data/WorldwideFramesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldwideFrames.Models;
+
+namespace WorldwideFrames.Data
+{
+    public class WorldwideFramesContext : DbContext
+    {
+        public WorldwideFramesContext(DbContextOptions<WorldwideFramesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Frame> Frame { get; set; }
+    }
+}
