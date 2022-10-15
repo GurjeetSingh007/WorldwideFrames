4a334de80061f8bcad9153b06978b8d90dd5d977
GurjeetSingh007 <W0792908@myscc.ca>
2022-10-15 14:15:52 -0700

    Added two route  in frame controller.

--- Controllers/FrameController.cs
+++ Controllers/FrameController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace WorldwideFrames.Controllers
 {
     public class FrameController : Controller
     {
-        public IActionResult Index()
+        
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+      
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
