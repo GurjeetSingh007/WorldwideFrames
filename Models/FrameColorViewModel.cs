1e6cbc725feaa9babd5d220348ff062937ac96c3
GurjeetSingh007 <W0792908@myscc.ca>
2022-10-16 20:27:09 -0700

    New function update for search bar.

---
+++ Models/FrameColorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace WorldwideFrames.Models
+{
+    public class FrameColorViewModel
+    {
+        public List<Frame> Frames { get; set; }
+        public SelectList Colors { get; set; }
+        public string FrameColor { get; set; }
+        public string SearchString { get; set; }
+    }
+}
